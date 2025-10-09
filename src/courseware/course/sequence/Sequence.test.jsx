bf7ae284ecc5de34891340740a150c46ebfd6155
Andrey Cañon <36200299+andrey-canon@users.noreply.github.com>
2025-10-09 11:35:12 -0500

    feat: add like-dislike component

--- src/courseware/course/sequence/Sequence.test.jsx
+++ src/courseware/course/sequence/Sequence.test.jsx
@@ -102,7 +102,7 @@ describe('Sequence', () => {
     waitFor(() => {
       expect(screen.queryByText('Loading locked content messaging...')).toBeInTheDocument();
       // `Previous`, `Prerequisite` and `Close Tray` buttons.
-      expect(screen.getAllByRole('button').length).toEqual(3);
+      expect(screen.getAllByRole('button').length).toEqual(5); // two more buttons like and dislike
       // `Next` button.
       expect(screen.getAllByRole('link').length).toEqual(1);
 
@@ -158,7 +158,7 @@ describe('Sequence', () => {
     waitFor(() => {
       expect(screen.findByText('Loading learning sequence...')).toBeInTheDocument();
       // `Previous`, `Prerequisite` and `Close Tray` buttons.
-      expect(screen.getAllByRole('button')).toHaveLength(3);
+      expect(screen.getAllByRole('button')).toHaveLength(5); // two more buttons like and dislike
       // Renders `Next` button.
       expect(screen.getAllByRole('link')).toHaveLength(1);
 
