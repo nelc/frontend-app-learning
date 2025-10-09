44bcd7cdbbc7ee44c185672688ec981127751869
Andrey Cañon <36200299+andrey-canon@users.noreply.github.com>
2025-10-09 11:35:12 -0500

    feat: add like-dislike component (#63)

--- src/courseware/course/sequence/Sequence.jsx
+++ src/courseware/course/sequence/Sequence.jsx
@@ -9,6 +9,7 @@ import {
 import { useIntl } from '@edx/frontend-platform/i18n';
 import { useSelector } from 'react-redux';
 import SequenceExamWrapper from '@edx/frontend-lib-special-exams';
+import { LikeDislikeUnit } from '@edunext/frontend-essentials';
 
 import PageLoading from '@src/generic/PageLoading';
 import { useModel } from '@src/generic/model-store';
@@ -206,7 +207,14 @@ const Sequence = ({
               isEnabledOutlineSidebar={isEnabledOutlineSidebar}
               renderUnitNavigation={renderUnitNavigation}
             />
-            {unitHasLoaded && renderUnitNavigation(false)}
+            {unitHasLoaded && (
+              <>
+                <div className="nelp-container">
+                  <LikeDislikeUnit courseId={courseId} unitId={unitId} />
+                </div>
+                {renderUnitNavigation(false)}
+              </>
+            )}
           </div>
         </div>
         <NotificationsDiscussionsSidebarSlot courseId={courseId} />
